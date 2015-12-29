1be55506e72cd4c92db376ffde7205da7dd0df7b
Edwin Villanueva <edwincv0@gmail.com>
2015-12-28 19:36:44 -0800

    Added shopping cart. Reviewed By: Edwin Villanueva

--- client/components/shop.jsx
+++ client/components/shop.jsx
@@ -4,11 +4,39 @@ Shop = React.createClass({
     componentWillMount(){
         Actions.setLayout('page');
     },
-
+    albumDetailCell(){
+        var songs = ["Bluebird",
+                "Mama's Sweet Potatoes",
+                "As It Is",
+                "A Little Stone On The Sidewalk",
+                "Crickets Night",
+                "Come Dance With Me",
+                "After the Rain",
+                "Magic Girl",
+                "Porchlight",
+                "Sugar Full Moon"
+            ];
+        var cellDetails = songs.map(function(val, idx){
+            return (<tr><td>{idx + 1 + ". "}</td><td>{val}</td></tr>)
+        });
+        return (
+            <tbody>{cellDetails}</tbody>
+        )
+    },
     render(){
         return (<div>
-            <div class="album-cover"></div>
-            <div class="album-detail"></div>
+            <div className="album-cover">
+                <img className="thumb-image" src="/images/album_cover.jpg"/>
+            </div>
+            <div className="album-detail">
+                <table>
+                    <tbody>
+                    {this.albumDetailCell()}
+                    </tbody>
+                </table>
+            </div>
+            <div className="paypal">
+            </div>
         </div>)
     }
 });
