b06fce21040f2f58ec37563b57c7a706bf3993c5
Edwin Villanueva <edwincv0@gmail.com>
2015-12-28 19:59:55 -0800

    Added album cover css. Reviewed By: Edwin Villanueva

--- client/components/shop.jsx
+++ client/components/shop.jsx
@@ -25,17 +25,27 @@ Shop = React.createClass({
     },
     render(){
         return (<div>
+            <h1 className="text-align-center">Shop</h1>
             <div className="album-cover">
-                <img className="thumb-image" src="/images/album_cover.jpg"/>
+                <img className="album-image" src="/images/album_cover.jpg"/>
             </div>
             <div className="album-detail">
                 <table>
-                    <tbody>
                     {this.albumDetailCell()}
-                    </tbody>
                 </table>
             </div>
-            <div className="paypal">
+            <div className="clear"></div>
+            <div className="text-align-center paypal">
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_new">
+                    <input name="cmd" value="_xclick" type="hidden"/>
+                    <input name="business" value="mienayoo@hotmail.com" type="hidden"/>
+                    <input name="item_name" value="a little stone on the sidewalk" type="hidden"/>
+                    <input name="item_number" value="" type="hidden"/>
+                    <input name="currency_code" value="USD" type="hidden"/>
+                    <input name="amount" value="14.99" type="hidden"/>
+                    <input name="no_note" value="1" type="hidden"/>
+                    <input alt="Make payments with PayPal - it's fast, free and secure!" name="submit" src="id2_files/btn_buy_now_b.gif" border="0" type="image"/>
+                </form>
             </div>
         </div>)
     }
