63c60578df6cccd885004ca787d3db5d887c9434
Edwin Villanueva <edwincv0@gmail.com>
2016-01-18 12:14:53 -0800

    Added music for autoplay

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -4,6 +4,12 @@ Home = React.createClass({
     },
 
     render(){
-        return (<div></div>)
+        return (<div>
+                    <audio autoPlay>
+                        <source src="index_files/endlessjourney.mp3" type="audio/mpeg">
+                            Your browser does not support the audio element.
+                        </source>
+                    </audio>
+                </div>)
     }
 });
