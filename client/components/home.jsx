993d53b56f7f64ca22a243802b6bc44a061b248e
Edwin Villanueva <edwincv0@gmail.com>
2016-02-07 12:02:43 -0800

    Added CDBaby Player

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -5,11 +5,6 @@ Home = React.createClass({
 
     render(){
         return (<div>
-                    <audio autoPlay>
-                        <source src="index_files/endlessjourney.mp3" type="audio/mpeg">
-                            Your browser does not support the audio element.
-                        </source>
-                    </audio>
                 </div>)
     }
 });
